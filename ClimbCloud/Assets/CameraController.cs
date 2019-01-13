36caa41e2d3cf58a2740396b91fb7bda72b949b2
techiela <hiro.green0@gmail.com>
2019-01-14 08:30:12 +0900

    add climbcloud

---
+++ ClimbCloud/Assets/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private GameObject player;
+
+    // Use this for initialization
+    void Start()
+    {
+        this.player = GameObject.Find("cat");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // カメラをプレイヤーのy座標に追従させる
+        Vector3 playerPos = this.player.transform.position;
+        transform.position = new Vector3(
+            transform.position.x,
+            playerPos.y,
+            transform.position.z);
+    }
+}
---
+++ ClimbCloud/Assets/PlayerController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rigid2D;
+    private Animator animator;
+    private float jumpForce = 680.0f;
+    private float walkForce = 30.0f;
+    private float maxWalkSpeed = 2.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        this.rigid2D = GetComponent<Rigidbody2D>();
+        this.animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // jump
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            this.rigid2D.AddForce(transform.up * this.jumpForce);
+        }
+
+        // move right/left
+        int key = 0;
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            key = 1;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            key = -1;
+        }
+
+        // speed of player
+        float speedx = Mathf.Abs(this.rigid2D.velocity.x);
+        // speed limit 
+        if (speedx < this.maxWalkSpeed)
+        {
+            this.rigid2D.AddForce(transform.right * key * this.walkForce);
+        }
+
+        // 振り向く
+        if (key != 0)
+        {
+            transform.localScale = new Vector3(key, 1, 1);
+        }
+
+        // プレイヤの速度に応じてアニメーション速度を変える
+        this.animator.speed = speedx / 2.0f;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("ゴール");
+    }
+}
