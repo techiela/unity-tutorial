6943b31b6cae2fc42baa571e640dbd6b5b821ea8
techiela <hiro.green0@gmail.com>
2019-04-27 10:03:42 +0900

    asis

--- ClimbCloud/Assets/PlayerController.cs
+++ ClimbCloud/Assets/PlayerController.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rigid2D;
     private Animator animator;
-    private float jumpForce = 680.0f;
+    private float jumpForce = 880.0f;
     private float walkForce = 30.0f;
     private float maxWalkSpeed = 2.0f;
 
@@ -21,7 +22,8 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // jump
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) &&
+            this.rigid2D.velocity.y == 0) // Y方向の速度が0 => ジャンプ中でない時
         {
             this.rigid2D.AddForce(transform.up * this.jumpForce);
         }
@@ -54,10 +56,17 @@ public class PlayerController : MonoBehaviour
 
         // プレイヤの速度に応じてアニメーション速度を変える
         this.animator.speed = speedx / 2.0f;
+        
+        // 落下したら最初から
+        if (transform.position.y < -10)
+        {
+            SceneManager.LoadScene("GameScene");
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log("ゴール");
+        SceneManager.LoadScene("ClearScene");
     }
 }
