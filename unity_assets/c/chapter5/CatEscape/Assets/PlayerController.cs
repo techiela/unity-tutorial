4e7db1265965c16770e566fd162233b7c926d181
techiela <hiro.green0@gmail.com>
2019-04-27 10:05:53 +0900

    add assets

---
+++ unity_assets/c/chapter5/CatEscape/Assets/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	void Start() {
+	}
+
+	void Update() {
+		// 左矢印が押された時
+		if(Input.GetKeyDown(KeyCode.LeftArrow)) {
+			transform.Translate(-3, 0, 0); // 左に「3」動かす
+		}
+
+		// 右矢印が押された時
+		if(Input.GetKeyDown(KeyCode.RightArrow)) {
+			transform.Translate(3, 0, 0); // 右に「3」動かす
+		}
+	}
+}
