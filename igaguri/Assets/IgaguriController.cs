81c27fa02e3a3d63fbf4d65650232023dfa28e54
techiela <hiro.green0@gmail.com>
2019-04-28 10:48:30 +0900

    igaguri

---
+++ igaguri/Assets/IgaguriController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgaguriController : MonoBehaviour
+{
+    // Use this for initialization
+    void Shoot(Vector3 dir)
+    {
+        GetComponent<Rigidbody>().AddForce(dir);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        GetComponent<Rigidbody>().isKinematic = true;
+        GetComponent<ParticleSystem>().Play();
+    }
+
+    void Start()
+    {
+        Shoot(new Vector3(0, 200, 2000));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
