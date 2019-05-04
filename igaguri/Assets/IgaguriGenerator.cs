f3f60f03d5959e6610e7f5426469e6c82bbeb761
techiela <hiro.green0@gmail.com>
2019-05-04 09:44:47 +0900

    section7 complete

--- igaguri/Assets/IgaguriGenerator.cs
+++ igaguri/Assets/IgaguriGenerator.cs
@@ -12,7 +12,10 @@ public class IgaguriGenerator : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             GameObject igaguri = Instantiate(igaguriPrefab) as GameObject;
-            igaguri.GetComponent<IgaguriController>().Shoot(new Vector3(0, 200, 2000));
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 worldDir = ray.direction;
+            igaguri.GetComponent<IgaguriController>().Shoot(worldDir.normalized * 2000);
         }
     }
 }
